86453c1a09fe26e91ba98b4ae93a222a0bab7ba3
Pratinav Agarwala <pratinavagarwa2006@gmail.com>
2021-01-28 21:07:12 +0530

    C78

--- Screens/WelcomeScreen.js
+++ Screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import * as  React from "react" ;
-import {View,Text,TextInput,TouchableOpacity,StyleSheet} from "react-native";
+import {View,Text,TextInput,TouchableOpacity,StyleSheet,Alert,Modal,ScrollView,KeyboardAvoidingView} from "react-native";
 import firebase from "firebase";
 import db from "../config";
 
@@ -8,7 +8,13 @@ export default class WelcomeScreen extends React.Component{
         super();
         this.state={
             emailId:"",
-            password:""
+            password:"",
+            isModalVisible:false,
+            firstName:"",
+            lastName:"",
+            phoneNo:"",
+            address:"",
+            confirmPassword:""
         }
     }
     userLogin=(emailId,password)=>{
@@ -19,9 +25,136 @@ export default class WelcomeScreen extends React.Component{
             return Alert.alert(error.message)
         })
     }
+
+    userSignUp=(emailId,password,confirmPassword)=>{
+        if(password!==confirmPassword){
+           return Alert.alert("Password does not match")
+        }else{
+            firebase.auth().createUserWithEmailAndPassword(emailId,password)
+            .then(()=>{
+                 
+                db.collection("users").add({
+                    firstName:this.state.firstName,
+                    lastName:this.state.lastName,
+                    phoneNo:this.state.phoneNo,
+                    address:this.state.address,
+                    emailId:this.state.emailId,
+                })
+                return Alert.alert("New user registered")
+            }).catch((error)=>{
+                return Alert.alert(error.message)
+                
+            })
+        }
+    }
+    showModal=()=>{
+        return(
+            <Modal animationType="fade" transparent={false} visible={this.state.isModalVisible}>
+                <View>
+                    <ScrollView style={{width:"100%",}}>
+                        <KeyboardAvoidingView>
+                            <Text>Registration</Text>
+                            <TextInput 
+                                placeholder="First Name"
+                                style={styles.inputBox}
+                                value={this.state.firstName}
+                                onChangeText={(text)=>{
+                                    this.setState({firstName:text})
+                                }}
+                            />
+
+                            <TextInput 
+                                placeholder="Last Name"
+                                style={styles.inputBox}
+                                value={this.state.lastName}
+                                onChangeText={(text)=>{
+                                    this.setState({lastName:text})
+                                }}
+                            />
+
+                            <TextInput 
+                                keyboardType="numeric"
+                                placeholder="Phone Number"
+                                style={styles.inputBox}
+                                value={this.state.phoneNo}
+                                onChangeText={(text)=>{
+                                    this.setState({phoneNo:text})
+                                }}
+                            />
+
+                            <TextInput 
+                                placeholder="Address"
+                                style={styles.inputBox}
+                                value={this.state.address}
+                                onChangeText={(text)=>{
+                                    this.setState({address:text})
+                                }}
+                            />
+
+                            <TextInput 
+                                style={styles.inputBox}
+                                placeholder="EmailId"
+                                keyboardType="email-address"
+                                onChangeText={(text)=>{
+                                    this.setState({
+                                        emailId:text
+                                    })
+                                }}
+                                value={
+                                    this.state.emailId
+                                }
+                            />
+
+                            <TextInput 
+                                style={styles.inputBox}
+                                placeholder="Password"
+                                secureTextEntry={true}
+                                onChangeText={(text)=>{
+                                    this.setState({
+                                        password:text
+                                    })
+                                }}
+                                value={
+                                    this.state.password
+                                }
+                            />
+
+                            <TextInput 
+                                style={styles.inputBox}
+                                placeholder="Confirm Password"
+                                secureTextEntry={true}
+                                onChangeText={(text)=>{
+                                    this.setState({
+                                        confirmPassword:text
+                                    })
+                                }}
+                                value={
+                                    this.state.confirmPassword
+                                }
+                            />
+
+                            <TouchableOpacity style={styles.button} onPress={()=>{
+                                this.userSignUp(this.state.emailId,this.state.password,this.state.confirmPassword)
+                            }}>
+                                <Text style={styles.text}>Register</Text>
+                            </TouchableOpacity>
+
+                            <TouchableOpacity style={styles.button} onPress={()=>{
+                                this.setState({isModalVisible:false})
+                            }}>
+                                <Text style={styles.text}>Cancel</Text>
+                            </TouchableOpacity>
+
+                        </KeyboardAvoidingView>
+                    </ScrollView>
+                </View>
+            </Modal>
+        )
+    }
     render(){
         return(
             <View style={styles.container}>
+                {this.showModal()}
                 <Text style={styles.text}>Book Santa</Text>
                 <TextInput 
                     style={styles.inputBox}
@@ -57,7 +190,9 @@ export default class WelcomeScreen extends React.Component{
                     <Text style={styles.text}>Login</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity style={styles.button} onPress={()=>{
+                    this.setState({isModalVisible:true})
+                }}>
                     <Text style={styles.text}>Sign Up</Text>
                 </TouchableOpacity>
             </View>
